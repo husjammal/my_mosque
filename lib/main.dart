a77cbf6dd9dd66490ca9d50f2bf1aabcd2601030
Hussam_Eddin_Al_Jammal <husjammal@gmail.com>
2023-03-25 19:15:53 +0000

    Adding Image

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:mymosque/app/auth/login.dart';
+import 'package:mymosque/app/auth/signup.dart';
+import 'package:mymosque/app/auth/success.dart';
+import 'package:mymosque/app/inistialScreen.dart';
+import 'package:mymosque/app/profilescreen.dart';
+import 'package:mymosque/app/splash.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(MyApp());
+late SharedPreferences sharedPref;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  sharedPref = await SharedPreferences.getInstance();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // Application name
-      title: 'Flutter Hello World',
+      title: 'My Mosque',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xff9a55f4),
+        textTheme: const TextTheme(bodyText2: TextStyle(color: Colors.purple)),
+        colorScheme:
+            ColorScheme.fromSwatch().copyWith(secondary: Colors.yellowAccent),
+        errorColor: Colors.yellow,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      initialRoute: sharedPref.getString("id") == null ? "login" : "splash",
+      routes: {
+        'login': (context) => Login(),
+        'signup': (context) => SignUp(),
+        'initialScreen': (context) => InitialScreen(),
+        'success': (context) => Success(),
+        'profilescreen': (context) => ProfileScreen(),
+        'splash': (context) => SplashPage(),
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+        // 'addnotes': (context) => AddNotes(),
+        // 'editnotes': (context) => EditNotes(),
+      },
     );
   }
 }
